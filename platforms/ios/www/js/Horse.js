ff55b7a4257fab486252677f6c436c2a202a552a
Soren Peterson <soren468@gmail.com>
2015-04-16 02:23:03 -0500

    Getting horse to stop when clicked

--- platforms/ios/www/js/Horse.js
+++ platforms/ios/www/js/Horse.js
@@ -4,23 +4,37 @@ var Horse = function() {
 
 Horse.prototype.render = function() {
   this.$el = $(new Image());
-  this.$el.attr('src', "img/horse_1@3x.png");
-  this.$el.attr('width', window.innerWidth/10);
-  this.$el.css('position', 'absolute');
+  this.styling();
+  this.bindEvents();
   this.moveImmediately();
   return this;
 }
 
+Horse.prototype.styling = function() {
+  this.$el.attr('src', "img/horse_1@3x.png");
+  this.$el.attr('width', window.innerWidth/10);
+  this.$el.css('position', 'absolute');
+}
+
+Horse.prototype.bindEvents = function() {
+  this.$el.on('click', function() {
+    console.log('clicked');
+  });
+}
+
 Horse.prototype.move = function() {
   var that = this;
-  setTimeout(function() {
+  var moving = setTimeout(function() {
     that.moveImmediately();
   }, 1000);
+  that.$el.on('click', function() {
+    clearTimeout(moving);
+  });
 }
 
 Horse.prototype.moveImmediately = function() {
   var that = this;
-  setTimeout(function() {
+  var moving = setTimeout(function() {
     var x = Math.random() * window.innerWidth;
     var y = Math.random() * window.innerHeight;
 
@@ -34,4 +48,7 @@ Horse.prototype.moveImmediately = function() {
 
     that.move();
   }, 0);
+  that.$el.on('click', function() {
+    clearTimeout(moving);
+  });
 }
