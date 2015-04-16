a54fb7205b4591795446d4718339963022247d7c
Soren Peterson <soren468@gmail.com>
2015-04-16 02:15:43 -0500

    Horse is running around on the screen

--- www/js/Horse.js
+++ www/js/Horse.js
@@ -5,10 +5,33 @@ var Horse = function() {
 Horse.prototype.render = function() {
   this.$el = $(new Image());
   this.$el.attr('src', "img/horse_1@3x.png");
-  this.$el.attr('width', window.innerWidth/10)
+  this.$el.attr('width', window.innerWidth/10);
+  this.$el.css('position', 'absolute');
+  this.moveImmediately();
   return this;
 }
 
 Horse.prototype.move = function() {
+  var that = this;
+  setTimeout(function() {
+    that.moveImmediately();
+  }, 1000);
+}
 
+Horse.prototype.moveImmediately = function() {
+  var that = this;
+  setTimeout(function() {
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+
+    console.log('moving...');
+    // something good here...
+    that.$el.animate({
+      left: x,
+      top: y
+    }, 1000);
+    // something good here...
+
+    that.move();
+  }, 0);
 }
