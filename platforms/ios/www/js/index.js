77a309d25a6fc9222cdaf4ffaedd844f7761887d
Soren Peterson <soren468@gmail.com>
2015-04-21 16:35:54 -0500

    Trying to switch to easel

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,2 +1,10 @@
 document.addEventListener('deviceready', function() {
+  var stage = new createjs.Stage("mainCanvas");
+  var horseSprite = new createjs.SpriteSheet({
+    images: ["./img/horse_1@3x.png"]
+  });
+  var sprite = new createjs.Sprite(horseSprite);
+  stage.addChild(sprite);
+  createjs.Ticker.on('tick', stage);
+  console.log('hello');
 });
