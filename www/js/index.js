2994975ff059cb5be2da73e79f03debc5cee29b6
Soren Peterson <soren468@gmail.com>
2015-04-21 00:16:43 -0500

    I'm now using a canvas

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,2 @@
 document.addEventListener('deviceready', function() {
-  setInterval(function() {
-    var horse = new Horse();
-    $('.container').append(horse.render().$el);
-  }, 1000);
-
-  FastClick.attach(document.body);
 });
