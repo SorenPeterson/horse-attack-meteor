fdaa8665e28ad0ef48a0709679945dfd84960b97
Soren Peterson <soren468@gmail.com>
2015-04-22 12:40:12 -0500

    Wow. This is working surprisingly well. Now I just need to unregister the click events

--- platforms/ios/www/js/Splash.js
+++ platforms/ios/www/js/Splash.js
@@ -1,6 +1,6 @@
 var Splash = function() {
   $('.container').html('');
-  $('.container').css('background-image', 'img/splash.png');
+  $('.container').css('background-image', 'url(img/splash.png)');
   $('.container').on('click', function() {
     Game();
   })
