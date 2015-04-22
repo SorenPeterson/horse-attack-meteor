8e14327bb6f4dcceccb3e9c44b88625ed59f8303
Soren Peterson <soren468@gmail.com>
2015-04-22 08:12:03 -0500

    Correctly sized canvas using JS

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,4 +1,7 @@
 document.addEventListener('deviceready', function() {
+  $("#mainCanvas").attr('width', window.innerWidth);
+  $("#mainCanvas").attr('height', window.innerHeight);
+
   var stage = new createjs.Stage("mainCanvas");
   var horse = new createjs.Bitmap('img/horse_1@3x.png');
   stage.addChild(horse);
