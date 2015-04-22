4ad759bf70df14976034f8d661ea479985992bed
Soren Peterson <soren468@gmail.com>
2015-04-21 23:35:00 -0500

    One bitmap on le screen

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,7 @@
 document.addEventListener('deviceready', function() {
   var stage = new createjs.Stage("mainCanvas");
-  var horseSprite = new createjs.SpriteSheet({
-    images: ["./img/horse_1@3x.png"]
-  });
-  var sprite = new createjs.Sprite(horseSprite);
-  stage.addChild(sprite);
+  var horse = new createjs.Bitmap('img/horse_1@3x.png');
+  stage.addChild(horse);
   createjs.Ticker.on('tick', stage);
   console.log('hello');
 });
