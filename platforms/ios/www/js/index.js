f9f21418c051735313841600e8be16fdde14edb1
Soren Peterson <soren468@gmail.com>
2015-04-22 09:02:26 -0500

    One single horse is moving around the screen

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,10 +1,28 @@
 document.addEventListener('deviceready', function() {
   $("#mainCanvas").attr('width', window.innerWidth);
-  $("#mainCanvas").attr('height', window.innerHeight);
+  $("#mainCanvas").attr('height', window.innerHeight-4);
 
   var stage = new createjs.Stage("mainCanvas");
   var horse = new createjs.Bitmap('img/horse_1@3x.png');
   stage.addChild(horse);
+
+  var moveHorse = function() {
+    previousx = horse.x;
+    previousy = horse.y;
+    nextx = Math.random() * window.innerWidth;
+    nexty = Math.random() * window.innerHeight;
+    var time = Math.sqrt(
+      Math.pow(nextx - previousx, 2) +
+      Math.pow(nexty - previousy, 2)
+    );
+    createjs.Tween.get(horse).to({
+      x: nextx,
+      y: nexty
+    }, time*2).call(moveHorse);
+  }
+
+  moveHorse();
+
   createjs.Ticker.on('tick', stage);
   console.log('hello');
 });
